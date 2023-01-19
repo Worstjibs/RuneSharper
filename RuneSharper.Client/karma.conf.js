c08ee981d7f6aac4553739e7e38dd5f31f44a254
morgan-rees <morgan_rees@live.co.uk>
2023-01-19 10:25:34 +0000

    Add Docker support to client

--- RuneSharper.Client/karma.conf.js
+++ RuneSharper.Client/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/rune-sharper.client'),
+      dir: require('path').join(__dirname, './coverage/runesharper-client'),
       subdir: '.',
       reporters: [
         { type: 'html' },
